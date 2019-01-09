40aecd500135c88eb94427c909a1f34e4fb3e392
Hu <hue@sec.gov>
2019-01-09 12:01:23 -0500

    Full Path

--- preload.el
+++ preload.el
@@ -2,5 +2,5 @@
 (defvar kitchingroup-github-id 'edwinhu)
 
 ;; PATHs
-(setenv "PATH" (concat "$HOME/anaconda3/bin:" "/usr/local/bin:" (getenv "PATH")))
-(setq exec-path (append '("$HOME/anaconda3/bin" "/usr/local/bin") exec-path))
+(setenv "PATH" (concat "/Users/hue/anaconda3/bin:" "/usr/local/bin:" (getenv "PATH")))
+(setq exec-path (append '("/Users/hue/anaconda3/bin" "/usr/local/bin") exec-path))
